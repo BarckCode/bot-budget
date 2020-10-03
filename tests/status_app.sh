9f952c95575346909dba5df8f82efe303daade03
BarckCode <barckcode@gmail.com>
2020-10-03 21:46:50 +0200

    BREAKING CHANGE: Add tests folder

---
+++ tests/status_app.sh
@@ -0,0 +1,24 @@
+# !/bin/bash
+# Description: Check Status App
+# Autor: Barckcode
+
+# Start App
+python3 main.py &
+sleep 2s
+
+# Look for the application process
+PID=$(ps -v | grep python3 | egrep -v grep | awk '{print $1}')
+
+# Check if it keeps running
+if [[ $PID -gt 0 ]]
+then
+    echo "El proceso de la app ha levantado con PID: $PID"
+    echo "El proceso morirá en 10s. Prueba la app desde Telegram"
+    sleep 5s
+    echo "El proceso morirá en 5s..."
+    sleep 5s
+    echo "Se va a matar la app con PID: $PID"
+    pkill python3
+else
+    echo "La app no ha levantado correctamente."
+fi
